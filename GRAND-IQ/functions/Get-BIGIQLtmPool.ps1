694bbb8f1fa9e656129f474ffbc40f51c05a1820
David Roberts <David.Roberts@americanfidelity.com>
2021-01-04 13:56:07 -0600

    Some help docs

--- GRAND-IQ/functions/Get-BIGIQLtmPool.ps1
+++ GRAND-IQ/functions/Get-BIGIQLtmPool.ps1
@@ -1,4 +1,23 @@
 function Get-BIGIQLtmPool {
+    <#
+    .SYNOPSIS
+    Get a pool from the local traffic management.
+    
+    .DESCRIPTION
+    Get a pool from the local traffic management
+    
+    .PARAMETER Partition
+    The partition the pool is in. Default is 'Common'.
+    
+    .PARAMETER PoolName
+    The exact name of the pool to get.
+    
+    .PARAMETER ExpandMembers
+    Indicates the cmdlet will expand the collection of pool members.
+    
+    .EXAMPLE
+    Get-BIGIQLtmPool -PoolName 'somepool' -ExpandMembers
+    #>
     [CmdletBinding()]
     param(
         [string]
@@ -22,18 +41,62 @@ function Get-BIGIQLtmPool {
 }
 
 function Remove-BIGIQLtmPoolMember {
+    <#
+    .SYNOPSIS
+    Removes a pool member.
+    
+    .DESCRIPTION
+    Removes a pool member.
+    
+    .PARAMETER PoolMember
+    The pool member object retrieved earlier having used pool members in a Get-BIGIQLtmPool response. Or the url to the pool member to remove.
+    
+    .EXAMPLE
+    $pool = Get-BIGIQLtmPool -PoolName somepool -ExpandMembers
+    Remove-BIGIQLtmPoolMember -PoolMember $pool.membersReference.items[0]
+
+    $pool = Get-BIGIQLtmPool -PoolName somepool -ExpandMembers
+    Remove-BIGIQLtmPoolMember -PoolMamber $pool.membersReference.items[0].selfLink
+    
+    .NOTES
+    This uses the 'selfLink' property of the provided pool member object.
+    #>
     [CmdletBinding()]
     param(
         [Parameter(Mandatory)]
         $PoolMember
     )
 
-    $response = Invoke-BIGIQRestRequest -Link $PoolMember.selfLink -Method delete
+    $uri = $null
+    if($PoolMember -is [string] -and (([Uri]$PoolMember).AbsoluteUri)
+    {
+        $uri = $PoolMember
+    } else {
+        $uri = $PoolMember.selfLink
+    }
+    $response = Invoke-BIGIQRestRequest -Link $uri -Method delete
 
     $response | Write-Output
 }
 
 function Add-BIGIQLtmPoolMember {
+    <#
+    .SYNOPSIS
+    Adds a node to an existing pool
+    
+    .DESCRIPTION
+    Adds a node to an existing pool
+    
+    .PARAMETER Pool
+    The pool object
+    
+    .PARAMETER Node
+    The node object to be added to the pool.
+    
+    .PARAMETER ServicePort
+    The network port the node responds to within the pool.
+
+    #>
     [CmdletBinding()]
     param(
         [Parameter(Mandatory)]
